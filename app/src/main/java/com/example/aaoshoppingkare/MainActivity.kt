6c0678987e91c254e62a9ffc928fa26139cd7b58
madhujeetTomar <madhujeet.tomar@gmail.com>
2023-01-06 11:45:51 +0530

    Added recyclerview and diffutils functionality

--- app/src/main/java/com/example/aaoshoppingkare/MainActivity.kt
+++ app/src/main/java/com/example/aaoshoppingkare/MainActivity.kt
@@ -5,6 +5,9 @@ import android.os.Bundle
 import android.util.Log
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.aaoshoppingkare.adapter.QuoteAdapter
 import com.example.aaoshoppingkare.viewmodel.QuoteViewModel
 import com.example.aaoshoppingkare.viewmodel.ViewModelFactory
 import javax.inject.Inject
@@ -15,13 +18,20 @@ class MainActivity : AppCompatActivity() {
     lateinit var viewModelProvider: ViewModelFactory
 
     private lateinit var viewModel: QuoteViewModel
+    private lateinit var recyclerView: RecyclerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        recyclerView = findViewById(R.id.recy_quotes)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.setHasFixedSize(true)
+        val quoteAdapter = QuoteAdapter()
         (application as ShopApp).appComponent.inject(this)
         viewModel = ViewModelProvider(this, viewModelProvider)[QuoteViewModel::class.java]
         viewModel.quotes.observe(this) { it ->
+            quoteAdapter.submitList(it.results)
+           recyclerView.adapter = quoteAdapter
             it.results.forEach { result ->
                 Log.d("Quotes: ", "${result.content} by ${result.author}")
             }
